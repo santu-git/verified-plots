36d38dc640045add04ef6f13543e49b25a5cc8a6
Santu Koley <santu.cst08@gmail.com>
2016-08-04 23:19:49 +0530

    map loading with mapbox done

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -22,6 +22,12 @@ export class ResultComponent implements OnInit {
       center: [51.505, -0.09],
       zoom: 13
     });
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox.streets',
+      accessToken: 'pk.eyJ1Ijoic2FudHVtYXAiLCJhIjoiY2lyZ2x5bnI5MDE0a2dka3g1dnIwd212cCJ9.vFLHKbovsmq76Y_aagzgLg'
+    }).addTo(map);
   }
 
 }
